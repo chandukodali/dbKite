3e3f913b425048cc00b29778666c7e261ea021a4
purna <chandu.kodali@gmail.com>
2019-08-21 15:59:16 +1000

    changed command

--- .buildkite/upload-pipeline.ps1
+++ .buildkite/upload-pipeline.ps1
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
 #
 # Upload pipeline steps to Buildkite
-buildkite-agent pipeline upload < "$(dirname $0)/pipeline.yml"
+# buildkite-agent pipeline upload < "$(dirname $0)/pipeline.yml"
+buildkite-agent pipeline upload .buildkite/pipeline.yml
