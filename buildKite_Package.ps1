ab87c80388c941b3b0f9e24ba0340be0b67f81e7
bkalliabetsou <brittany.kalliabetsou@hometrack.com.au>
2019-08-22 14:05:06 +1000

    Update buildKite_Package.ps1

--- buildKite_Package.ps1
+++ buildKite_Package.ps1
@@ -6,10 +6,11 @@ Write-Host "Starting Package Creation"
 #re-validate
 $scriptsFolder = "C:\buildkite-agent\builds\red-sye3-01-1\dbkite-1\db-pipeline\redgate"
 $validatedScriptsFolder = Invoke-DlmDatabaseSchemaValidation $scriptsFolder
+$packagePath = "C:\buildkite-agent\packages"
 
 Write-Output $latestFileNameSubstring1+$fileVersion
 
-$latestFileName = Get-ChildItem -Path $scriptsFolder -Recurse | group directory | foreach {$_.group | sort LastWriteTime -Descending | select -First 1} | Select
+$latestFileName = Get-ChildItem -Path $packagePath -Recurse | group directory | foreach {$_.group | sort LastWriteTime -Descending | select -First 1} | Select
 $latestFileNameSubstring1 = $latestFileName.Name.Substring(0,11)
 $fileVersion = $latestFileName.Name.Substring(11,1)
 $fileVersion = [int]$fileVersion + 1
