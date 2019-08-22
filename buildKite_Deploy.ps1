844ba173ad1056219a415f01ba0fa361ef11b598
bkalliabetsou <brittany.kalliabetsou@hometrack.com.au>
2019-08-22 15:10:15 +1000

    Addition of deploy powershell script

---
+++ buildKite_Deploy.ps1
@@ -0,0 +1,20 @@
+﻿#DEPLOY
+TRY
+{
+$destination = New-DlmDatabaseConnection -ServerInstance "172.22.52.53,5026" -Database "DBKite" -Username "build_admin" -Password "HomeTrack123!!"
+$dbRelease = New-DlmDatabaseRelease -Source "C:\buildkite-agent\packages\DBKite.1.0.5.nupkg" -Target $destination
+
+#Export scripts
+write-host "Exporting deployment scripts"
+Export-DlmDatabaseRelease $dbRelease -Path "C:\buildkite-agent\releaseScripts" 
+
+#Do the deployment
+write-host "Beginning deployment"
+Use-DlmDatabaseRelease $dbRelease -DeployTo $destination
+write-host "Deployment Complete"
+exit 0
+}
+catch
+{
+exit 1
+}
